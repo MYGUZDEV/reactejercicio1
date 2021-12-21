556c4f7e2bce752b7ef0feda4f1a04e245a20773
PABLO <pablo.d.guzman.1@gmail.com>
2021-12-20 22:10:49 -0300

    ejercicio1 a revisar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import '../assets/css/app.css';
+import SideBar from './SideBar';
+import ContentWappred from './ContentWappred';
+//import "../assets/images"
+
+function App() {
+  return (
+    <><SideBar/><ContentWappred/></>
+    );
+}
+
+export default App;
---
+++ src/components/ContentWappred.js
@@ -0,0 +1,9 @@
+import TopBar from './TopBar';
+import Footer from './Footer';
+import ContentRowTop from './ContentRowTop';
+
+function ContentWappred () {
+    <><TopBar/><Footer/><ContentRowTop/></>
+};
+
+export default ContentWappred;
